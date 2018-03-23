09c878b6483b6fecc694e3cd269576e4a8afe9de
Rasmus Henningsson <Rasmus.Henningsson@gmail.com>
2018-03-23 10:09:26 +0800

    Added Colors requirement. Added warnings for missing dependencies.

--- src/AlignUtils.jl
+++ src/AlignUtils.jl
@@ -1,7 +1,14 @@
 module AlignUtils
 
+Pkg.installed("DISSEQT")==nothing && warn("Module DISSEQT not installed. Please refer to AlignUtils installation instructions at https://github.com/rasmushenningsson/AlignUtils.jl")
+Pkg.installed("SynapseClient")==nothing && warn("Module SynapseClient not installed. Please refer to AlignUtils installation instructions at https://github.com/rasmushenningsson/AlignUtils.jl")
+Pkg.installed("SynapseTools")==nothing && warn("Module SynapseTools not installed. Please refer to AlignUtils installation instructions at https://github.com/rasmushenningsson/AlignUtils.jl")
+Pkg.installed("BamReader")==nothing && warn("Module BamReader not installed. Please refer to AlignUtils installation instructions at https://github.com/rasmushenningsson/AlignUtils.jl")
+
+
 using DataFrames
 using Gadfly
+using Colors
 using JLD
 Pkg.installed("MAT")!=nothing && using MAT
 using Levenshtein
